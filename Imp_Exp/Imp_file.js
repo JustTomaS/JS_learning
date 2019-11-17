2c46a11ba76b7cdfe8ed0c1c72e3a8663045dc10
JustTomaS <FirstofDen@yandex.ru>
2019-11-18 03:12:17 +0700

    Delete Imp_file.js

--- Imp_Exp/Imp_file.js
+++
@@ -1,7 +0,0 @@
-/* Импортируем свойство объекта в данный файл*/
-
-require (`./Exp_file.js`);
-
-console.log(`У меня есть ` + apple.device1 + ` с памятью ` + apple.memory1);
-
-console.log(`Но я хочу купить ` + apple.device2 + ` с цветом ` + apple.color2);
